5e0dbabe3b6b3080327c58461bd06c2563b6dc0c
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-20 20:59:35 +0000

    My copies of today's homework. I'll be forking Lauren's work.

--- week_01/day_3/homework/arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ week_01/day_3/homework/arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -4,42 +4,42 @@ users = {
     :lottery_numbers => [6, 12, 49, 33, 45, 20],
     :home_town => "Stirling",
     :pets => [
-    {
-      :name => "fluffy",
-      :species => "cat"
-    },
-    {
-      :name => "fido",
-      :species => "dog"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    }
-  ]
+      {
+        :name => "fluffy",
+        :species => "cat"
+      },
+      {
+        :name => "fido",
+        :species => "dog"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      }
+    ]
   },
   "Erik" => {
     :twitter => "eriksf",
     :lottery_numbers => [18, 34, 8, 11, 24],
     :home_town => "Linlithgow",
     :pets => [
-    {
-      :name => "nemo",
-      :species => "fish"
-    },
-    {
-      :name => "kevin",
-      :species => "fish"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    },
-    {
-      :name => "rupert",
-      :species => "parrot"
-    }
-  ]
+      {
+        :name => "nemo",
+        :species => "fish"
+      },
+      {
+        :name => "kevin",
+        :species => "fish"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      },
+      {
+        :name => "rupert",
+        :species => "parrot"
+      }
+    ]
   },
   "Avril" => {
     :twitter => "bridgpally",
@@ -55,10 +55,15 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users ["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users ["Erik"] [:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
+p users ["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers]
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
